3ee572e572b89d09f61d2c452dbf7f20e2a3d019
Drew-boi <122581219+Drew-boi@users.noreply.github.com>
2024-07-21 21:16:27 -0700

    Added in conversion from 4 digit codes to 6 digit codes
    I used test 5 to do this, as the first line is a regular number and the second line is an instruction.

--- input_handler.cpp
+++ input_handler.cpp
@@ -3,6 +3,55 @@
 #include <iomanip>
 #include <stdexcept>
 #include <sstream>
+#include <cctype>
+
+bool isInstr(std::string& instruction){
+    bool is_instruction = false;
+    int numeric_instruction = std::stoi(instruction);
+    int op_code = numeric_instruction / 100;
+    switch (op_code) {
+        case 10: // 10: READ
+            is_instruction = true;
+            break;
+        case 11: // 11: WRITE
+            is_instruction = true;
+            break;
+        case 20: // 20: LOAD
+            is_instruction = true;
+            break;
+        case 21: // 21: STORE
+            is_instruction = true;
+            break;
+        case 30: // 30: ADD
+            is_instruction = true;
+            break;
+        case 31: // 31: SUBTRACT
+            is_instruction = true;
+            break;
+        case 32: // 32: DIVIDE
+            is_instruction = true;
+            break;
+        case 33: // 33: MULTIPLY
+            is_instruction = true;
+            break;
+        case 40: // 40: BRANCH
+            is_instruction = true;
+            break;
+        case 41: // 41: BRANCHNEG
+            is_instruction = true;
+            break;
+        case 42: // 42: BRANCHZERO
+            is_instruction = true;
+            break;
+        case 43: // 43: HALT
+            is_instruction = true;
+            break;
+        default: // INVALID OPCODE
+            break;
+    }
+    return is_instruction;
+}
+
 
 std::vector<std::string> InputHandler::split_lines(std::istream& is) {
     if (!is) {
@@ -56,6 +105,7 @@ void InputHandler::validate_instruction(std::string& instruction) {
 }
 
 void InputHandler::validate_format(std::string& instruction) {
+    instruction.erase(std::remove_if(instruction.begin(), instruction.end(), ::isspace), instruction.end());
     // Handling for instruction empty string
     if (instruction.empty()) {
         throw std::invalid_argument("Validation Error: Instruction is empty.\n");
@@ -67,21 +117,21 @@ void InputHandler::validate_format(std::string& instruction) {
         char c = instruction[0];
         if (!isdigit(c)) {
             throw std::invalid_argument(
-                "Validation Error: Invalid format '" + instruction + "'. Must be a sequence of 4 digits.");
+                "Validation Error: Invalid format '" + instruction + "'. Must be a sequence of 4 or 6 digits.");
         }
     }
 
-    // Handling for instruction string size greater than 4 char with bad format or greater than 5 char with good format
+    // Handling for instruction string size greater than 6 char with bad format or greater than 7 char with good format
     bool size_warning = false;
-    if (instruction_length > 4) {
+    if (instruction_length >= 6) {
         if (instruction[0] == '-' || instruction[0] == '+') {
-            // Only keep the first 5 characters
-            instruction = instruction.substr(0, 5);
-            size_warning = (instruction_length > 5) ? true : false;
+            // Only keep the first 7 characters
+            instruction = instruction.substr(0, 7);
+            size_warning = (instruction_length > 7) ? true : false;
         } else {
-            // Only keep the first 4 characters
-            instruction = instruction.substr(0, 4);
-            size_warning = (instruction_length > 4) ? true : false;
+            // Only keep the first 6 characters
+            instruction = instruction.substr(0, 6);
+            size_warning = (instruction_length > 6) ? true : false;
         }
     }
     if (size_warning) {
@@ -90,7 +140,43 @@ void InputHandler::validate_format(std::string& instruction) {
                 << instruction << "' from '" << original_instruction << "' will attempt to be stored.\n";
         throw std::length_error(warning.str());
     }
-
+    // Check if instruction is a 4 digit instruction
+    if (instruction_length >= 4 and instruction_length < 6) {
+        bool is_instruction;
+        std::string temp_instruction;
+        if (instruction[0] == '-' || instruction[0] == '+') {
+            temp_instruction = instruction.substr(1, 4);
+        }
+        else {
+            temp_instruction = instruction.substr(0, 4);
+        }
+        is_instruction = ::isInstr(temp_instruction);
+        if (is_instruction) {
+            std::string zero = "0";
+            if (instruction[0] == '-' || instruction[0] == '+') {
+                std::string instruction_sign = instruction.substr(0, 1);
+                std::string instruction_bits = instruction.substr(1, 2);
+                std::string instruction_mem = instruction.substr(3, 2);
+                instruction = instruction_sign + zero + instruction_bits + zero + instruction_mem;
+            }
+            else {
+                std::string instruction_bits = instruction.substr(0, 2);
+                std::string instruction_mem = instruction.substr(2, 2);
+                instruction = zero + instruction_bits + zero + instruction_mem;
+            }
+        }
+        else {
+            std::string zeroes = "00";
+            if (instruction[0] == '-' || instruction[0] == '+') {
+                std::string instruction_sign = instruction.substr(0, 1);
+                instruction = instruction.substr(1, 4);
+                instruction = instruction_sign + zeroes + instruction;
+            }
+            else {
+                instruction = zeroes + instruction;
+            }
+        }
+    }
     // Validate each character in the instruction string
     for (size_t i = 0; i < instruction_length; ++i) {
         char c = instruction[i];
@@ -112,7 +198,7 @@ void InputHandler::validate_numeric(std::string& instruction) {
         int numeric_instruction = std::stoi(instruction);
         std::ostringstream os;
         os << (numeric_instruction >= 0 ? '+' : '-');
-        os << std::setw(4) << std::setfill('0') << std::abs(numeric_instruction);
+        os << std::setw(6) << std::setfill('0') << std::abs(numeric_instruction);
         instruction = os.str();
     } catch (const std::invalid_argument& stoi_err) {
         std::ostringstream throw_context;
